054eb139338553ca89e9b5d1b7da2d2dcfcdec91
drefas <drefasgt@gmail.com>
2025-04-07 22:37:47 -0700

    Add files via upload

--- decorators.py
+++ decorators.py
@@ -1,136 +1,225 @@
-import asyncio
-from functools import wraps
-from urllib.parse import urlparse
+"Functions that help with dynamically creating decorators for views."
 
-from asgiref.sync import async_to_sync, sync_to_async
+from functools import partial, update_wrapper, wraps
 
-from django.conf import settings
-from django.contrib.auth import REDIRECT_FIELD_NAME
-from django.core.exceptions import PermissionDenied
-from django.shortcuts import resolve_url
+from asgiref.sync import iscoroutinefunction
 
 
-def user_passes_test(
-    test_func, login_url=None, redirect_field_name=REDIRECT_FIELD_NAME
-):
+class classonlymethod(classmethod):
+    def __get__(self, instance, cls=None):
+        if instance is not None:
+            raise AttributeError(
+                "This method is available only on the class, not on instances."
+            )
+        return super().__get__(instance, cls)
+
+
+def _update_method_wrapper(_wrapper, decorator):
+    # _multi_decorate()'s bound_method isn't available in this scope. Cheat by
+    # using it on a dummy function.
+    @decorator
+    def dummy(*args, **kwargs):
+        pass
+
+    update_wrapper(_wrapper, dummy)
+
+
+def _multi_decorate(decorators, method):
     """
-    Decorator for views that checks that the user passes the given test,
-    redirecting to the log-in page if necessary. The test should be a callable
-    that takes the user object and returns True if the user passes.
+    Decorate `method` with one or more function decorators. `decorators` can be
+    a single decorator or an iterable of decorators.
     """
-
-    def decorator(view_func):
-        def _redirect_to_login(request):
-            path = request.build_absolute_uri()
-            resolved_login_url = resolve_url(login_url or settings.LOGIN_URL)
-            # If the login url is the same scheme and net location then just
-            # use the path as the "next" url.
-            login_scheme, login_netloc = urlparse(resolved_login_url)[:2]
-            current_scheme, current_netloc = urlparse(path)[:2]
-            if (not login_scheme or login_scheme == current_scheme) and (
-                not login_netloc or login_netloc == current_netloc
-            ):
-                path = request.get_full_path()
-            from django.contrib.auth.views import redirect_to_login
-
-            return redirect_to_login(path, resolved_login_url, redirect_field_name)
-
-        if asyncio.iscoroutinefunction(view_func):
-
-            async def _view_wrapper(request, *args, **kwargs):
-                auser = await request.auser()
-                if asyncio.iscoroutinefunction(test_func):
-                    test_pass = await test_func(auser)
-                else:
-                    test_pass = await sync_to_async(test_func)(auser)
-
-                if test_pass:
-                    return await view_func(request, *args, **kwargs)
-                return _redirect_to_login(request)
-
-        else:
-
-            def _view_wrapper(request, *args, **kwargs):
-                if asyncio.iscoroutinefunction(test_func):
-                    test_pass = async_to_sync(test_func)(request.user)
-                else:
-                    test_pass = test_func(request.user)
-
-                if test_pass:
-                    return view_func(request, *args, **kwargs)
-                return _redirect_to_login(request)
-
-        # Attributes used by LoginRequiredMiddleware.
-        _view_wrapper.login_url = login_url
-        _view_wrapper.redirect_field_name = redirect_field_name
-
-        return wraps(view_func)(_view_wrapper)
-
-    return decorator
-
-
-def login_required(
-    function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None
-):
-    """
-    Decorator for views that checks that the user is logged in, redirecting
-    to the log-in page if necessary.
-    """
-    actual_decorator = user_passes_test(
-        lambda u: u.is_authenticated,
-        login_url=login_url,
-        redirect_field_name=redirect_field_name,
-    )
-    if function:
-        return actual_decorator(function)
-    return actual_decorator
-
-
-def login_not_required(view_func):
-    """
-    Decorator for views that allows access to unauthenticated requests.
-    """
-    view_func.login_required = False
-    return view_func
-
-
-def permission_required(perm, login_url=None, raise_exception=False):
-    """
-    Decorator for views that checks whether a user has a particular permission
-    enabled, redirecting to the log-in page if necessary.
-    If the raise_exception parameter is given the PermissionDenied exception
-    is raised.
-    """
-    if isinstance(perm, str):
-        perms = (perm,)
+    if hasattr(decorators, "__iter__"):
+        # Apply a list/tuple of decorators if 'decorators' is one. Decorator
+        # functions are applied so that the call order is the same as the
+        # order in which they appear in the iterable.
+        decorators = decorators[::-1]
     else:
-        perms = perm
+        decorators = [decorators]
 
-    def decorator(view_func):
-        if asyncio.iscoroutinefunction(view_func):
+    def _wrapper(self, *args, **kwargs):
+        # bound_method has the signature that 'decorator' expects i.e. no
+        # 'self' argument, but it's a closure over self so it can call
+        # 'func'. Also, wrap method.__get__() in a function because new
+        # attributes can't be set on bound method objects, only on functions.
+        bound_method = wraps(method)(partial(method.__get__(self, type(self))))
+        for dec in decorators:
+            bound_method = dec(bound_method)
+        return bound_method(*args, **kwargs)
 
-            async def check_perms(user):
-                # First check if the user has the permission (even anon users).
-                if await sync_to_async(user.has_perms)(perms):
-                    return True
-                # In case the 403 handler should be called raise the exception.
-                if raise_exception:
-                    raise PermissionDenied
-                # As the last resort, show the login form.
-                return False
+    # Copy any attributes that a decorator adds to the function it decorates.
+    for dec in decorators:
+        _update_method_wrapper(_wrapper, dec)
+    # Preserve any existing attributes of 'method', including the name.
+    update_wrapper(_wrapper, method)
+    return _wrapper
 
-        else:
 
-            def check_perms(user):
-                # First check if the user has the permission (even anon users).
-                if user.has_perms(perms):
-                    return True
-                # In case the 403 handler should be called raise the exception.
-                if raise_exception:
-                    raise PermissionDenied
-                # As the last resort, show the login form.
-                return False
+def method_decorator(decorator, name=""):
+    """
+    Convert a function decorator into a method decorator
+    """
 
-        return user_passes_test(check_perms, login_url=login_url)(view_func)
+    # 'obj' can be a class or a function. If 'obj' is a function at the time it
+    # is passed to _dec,  it will eventually be a method of the class it is
+    # defined on. If 'obj' is a class, the 'name' is required to be the name
+    # of the method that will be decorated.
+    def _dec(obj):
+        if not isinstance(obj, type):
+            return _multi_decorate(decorator, obj)
+        if not (name and hasattr(obj, name)):
+            raise ValueError(
+                "The keyword argument `name` must be the name of a method "
+                "of the decorated class: %s. Got '%s' instead." % (obj, name)
+            )
+        method = getattr(obj, name)
+        if not callable(method):
+            raise TypeError(
+                "Cannot decorate '%s' as it isn't a callable attribute of "
+                "%s (%s)." % (name, obj, method)
+            )
+        _wrapper = _multi_decorate(decorator, method)
+        setattr(obj, name, _wrapper)
+        return obj
 
-    return decorator
+    # Don't worry about making _dec look similar to a list/tuple as it's rather
+    # meaningless.
+    if not hasattr(decorator, "__iter__"):
+        update_wrapper(_dec, decorator)
+    # Change the name to aid debugging.
+    obj = decorator if hasattr(decorator, "__name__") else decorator.__class__
+    _dec.__name__ = "method_decorator(%s)" % obj.__name__
+    return _dec
+
+
+def decorator_from_middleware_with_args(middleware_class):
+    """
+    Like decorator_from_middleware, but return a function
+    that accepts the arguments to be passed to the middleware_class.
+    Use like::
+
+         cache_page = decorator_from_middleware_with_args(CacheMiddleware)
+         # ...
+
+         @cache_page(3600)
+         def my_view(request):
+             # ...
+    """
+    return make_middleware_decorator(middleware_class)
+
+
+def decorator_from_middleware(middleware_class):
+    """
+    Given a middleware class (not an instance), return a view decorator. This
+    lets you use middleware functionality on a per-view basis. The middleware
+    is created with no params passed.
+    """
+    return make_middleware_decorator(middleware_class)()
+
+
+def make_middleware_decorator(middleware_class):
+    def _make_decorator(*m_args, **m_kwargs):
+        def _decorator(view_func):
+            middleware = middleware_class(view_func, *m_args, **m_kwargs)
+
+            def _pre_process_request(request, *args, **kwargs):
+                if hasattr(middleware, "process_request"):
+                    result = middleware.process_request(request)
+                    if result is not None:
+                        return result
+                if hasattr(middleware, "process_view"):
+                    result = middleware.process_view(request, view_func, args, kwargs)
+                    if result is not None:
+                        return result
+                return None
+
+            def _process_exception(request, exception):
+                if hasattr(middleware, "process_exception"):
+                    result = middleware.process_exception(request, exception)
+                    if result is not None:
+                        return result
+                raise
+
+            def _post_process_request(request, response):
+                if hasattr(response, "render") and callable(response.render):
+                    if hasattr(middleware, "process_template_response"):
+                        response = middleware.process_template_response(
+                            request, response
+                        )
+                    # Defer running of process_response until after the template
+                    # has been rendered:
+                    if hasattr(middleware, "process_response"):
+
+                        def callback(response):
+                            return middleware.process_response(request, response)
+
+                        response.add_post_render_callback(callback)
+                else:
+                    if hasattr(middleware, "process_response"):
+                        return middleware.process_response(request, response)
+                return response
+
+            if iscoroutinefunction(view_func):
+
+                async def _view_wrapper(request, *args, **kwargs):
+                    result = _pre_process_request(request, *args, **kwargs)
+                    if result is not None:
+                        return result
+
+                    try:
+                        response = await view_func(request, *args, **kwargs)
+                    except Exception as e:
+                        result = _process_exception(request, e)
+                        if result is not None:
+                            return result
+
+                    return _post_process_request(request, response)
+
+            else:
+
+                def _view_wrapper(request, *args, **kwargs):
+                    result = _pre_process_request(request, *args, **kwargs)
+                    if result is not None:
+                        return result
+
+                    try:
+                        response = view_func(request, *args, **kwargs)
+                    except Exception as e:
+                        result = _process_exception(request, e)
+                        if result is not None:
+                            return result
+
+                    return _post_process_request(request, response)
+
+            return wraps(view_func)(_view_wrapper)
+
+        return _decorator
+
+    return _make_decorator
+
+
+def sync_and_async_middleware(func):
+    """
+    Mark a middleware factory as returning a hybrid middleware supporting both
+    types of request.
+    """
+    func.sync_capable = True
+    func.async_capable = True
+    return func
+
+
+def sync_only_middleware(func):
+    """
+    Mark a middleware factory as returning a sync middleware.
+    This is the default.
+    """
+    func.sync_capable = True
+    func.async_capable = False
+    return func
+
+
+def async_only_middleware(func):
+    """Mark a middleware factory as returning an async middleware."""
+    func.sync_capable = False
+    func.async_capable = True
+    return func
--- log.py
+++ log.py
@@ -1,67 +1,257 @@
-from django import template
+import logging
+import logging.config  # needed when logging_config doesn't start with logging.config
+from copy import copy
 
-register = template.Library()
+from django.conf import settings
+from django.core import mail
+from django.core.mail import get_connection
+from django.core.management.color import color_style
+from django.utils.module_loading import import_string
+
+request_logger = logging.getLogger("django.request")
+
+# Default logging for Django. This sends an email to the site admins on every
+# HTTP 500 error. Depending on DEBUG, all other log records are either sent to
+# the console (DEBUG=True) or discarded (DEBUG=False) by means of the
+# require_debug_true filter. This configuration is quoted in
+# docs/ref/logging.txt; please amend it there if edited here.
+DEFAULT_LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
+        "django.server": {
+            "()": "django.utils.log.ServerFormatter",
+            "format": "[{server_time}] {message}",
+            "style": "{",
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+        },
+        "django.server": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "django.server",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "mail_admins"],
+            "level": "INFO",
+        },
+        "django.server": {
+            "handlers": ["django.server"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
 
 
-class AdminLogNode(template.Node):
-    def __init__(self, limit, varname, user):
-        self.limit = limit
-        self.varname = varname
-        self.user = user
+def configure_logging(logging_config, logging_settings):
+    if logging_config:
+        # First find the logging configuration function ...
+        logging_config_func = import_string(logging_config)
 
-    def __repr__(self):
-        return "<GetAdminLog Node>"
+        logging.config.dictConfig(DEFAULT_LOGGING)
 
-    def render(self, context):
-        entries = context["log_entries"]
-        if self.user is not None:
-            user_id = self.user
-            if not user_id.isdigit():
-                user_id = context[self.user].pk
-            entries = entries.filter(user__pk=user_id)
-        context[self.varname] = entries[: int(self.limit)]
-        return ""
+        # ... then invoke it with the logging settings
+        if logging_settings:
+            logging_config_func(logging_settings)
 
 
-@register.tag
-def get_admin_log(parser, token):
+class AdminEmailHandler(logging.Handler):
+    """An exception log handler that emails log entries to site admins.
+
+    If the request is passed as the first argument to the log record,
+    request data will be provided in the email report.
     """
-    Populate a template variable with the admin log for the given criteria.
 
-    Usage::
-
-        {% get_admin_log [limit] as [varname] for_user [context_var_with_user_obj] %}
-
-    Examples::
-
-        {% get_admin_log 10 as admin_log for_user 23 %}
-        {% get_admin_log 10 as admin_log for_user user %}
-        {% get_admin_log 10 as admin_log %}
-
-    Note that ``context_var_containing_user_obj`` can be a hard-coded integer
-    (user ID) or the name of a template context variable containing the user
-    object whose ID you want.
-    """
-    tokens = token.contents.split()
-    if len(tokens) < 4:
-        raise template.TemplateSyntaxError(
-            "'get_admin_log' statements require two arguments"
+    def __init__(self, include_html=False, email_backend=None, reporter_class=None):
+        super().__init__()
+        self.include_html = include_html
+        self.email_backend = email_backend
+        self.reporter_class = import_string(
+            reporter_class or settings.DEFAULT_EXCEPTION_REPORTER
         )
-    if not tokens[1].isdigit():
-        raise template.TemplateSyntaxError(
-            "First argument to 'get_admin_log' must be an integer"
-        )
-    if tokens[2] != "as":
-        raise template.TemplateSyntaxError(
-            "Second argument to 'get_admin_log' must be 'as'"
-        )
-    if len(tokens) > 4:
-        if tokens[4] != "for_user":
-            raise template.TemplateSyntaxError(
-                "Fourth argument to 'get_admin_log' must be 'for_user'"
+
+    def emit(self, record):
+        # Early return when no email will be sent.
+        if (
+            not settings.ADMINS
+            # Method not overridden.
+            and self.send_mail.__func__ is AdminEmailHandler.send_mail
+        ):
+            return
+        try:
+            request = record.request
+            subject = "%s (%s IP): %s" % (
+                record.levelname,
+                (
+                    "internal"
+                    if request.META.get("REMOTE_ADDR") in settings.INTERNAL_IPS
+                    else "EXTERNAL"
+                ),
+                record.getMessage(),
             )
-    return AdminLogNode(
-        limit=tokens[1],
-        varname=tokens[3],
-        user=(tokens[5] if len(tokens) > 5 else None),
+        except Exception:
+            subject = "%s: %s" % (record.levelname, record.getMessage())
+            request = None
+        subject = self.format_subject(subject)
+
+        # Since we add a nicely formatted traceback on our own, create a copy
+        # of the log record without the exception data.
+        no_exc_record = copy(record)
+        no_exc_record.exc_info = None
+        no_exc_record.exc_text = None
+
+        if record.exc_info:
+            exc_info = record.exc_info
+        else:
+            exc_info = (None, record.getMessage(), None)
+
+        reporter = self.reporter_class(request, is_email=True, *exc_info)
+        message = "%s\n\n%s" % (
+            self.format(no_exc_record),
+            reporter.get_traceback_text(),
+        )
+        html_message = reporter.get_traceback_html() if self.include_html else None
+        self.send_mail(subject, message, fail_silently=True, html_message=html_message)
+
+    def send_mail(self, subject, message, *args, **kwargs):
+        mail.mail_admins(
+            subject, message, *args, connection=self.connection(), **kwargs
+        )
+
+    def connection(self):
+        return get_connection(backend=self.email_backend, fail_silently=True)
+
+    def format_subject(self, subject):
+        """
+        Escape CR and LF characters.
+        """
+        return subject.replace("\n", "\\n").replace("\r", "\\r")
+
+
+class CallbackFilter(logging.Filter):
+    """
+    A logging filter that checks the return value of a given callable (which
+    takes the record-to-be-logged as its only parameter) to decide whether to
+    log a record.
+    """
+
+    def __init__(self, callback):
+        self.callback = callback
+
+    def filter(self, record):
+        if self.callback(record):
+            return 1
+        return 0
+
+
+class RequireDebugFalse(logging.Filter):
+    def filter(self, record):
+        return not settings.DEBUG
+
+
+class RequireDebugTrue(logging.Filter):
+    def filter(self, record):
+        return settings.DEBUG
+
+
+class ServerFormatter(logging.Formatter):
+    default_time_format = "%d/%b/%Y %H:%M:%S"
+
+    def __init__(self, *args, **kwargs):
+        self.style = color_style()
+        super().__init__(*args, **kwargs)
+
+    def format(self, record):
+        msg = record.msg
+        status_code = getattr(record, "status_code", None)
+
+        if status_code:
+            if 200 <= status_code < 300:
+                # Put 2XX first, since it should be the common case
+                msg = self.style.HTTP_SUCCESS(msg)
+            elif 100 <= status_code < 200:
+                msg = self.style.HTTP_INFO(msg)
+            elif status_code == 304:
+                msg = self.style.HTTP_NOT_MODIFIED(msg)
+            elif 300 <= status_code < 400:
+                msg = self.style.HTTP_REDIRECT(msg)
+            elif status_code == 404:
+                msg = self.style.HTTP_NOT_FOUND(msg)
+            elif 400 <= status_code < 500:
+                msg = self.style.HTTP_BAD_REQUEST(msg)
+            else:
+                # Any 5XX, or any other status code
+                msg = self.style.HTTP_SERVER_ERROR(msg)
+
+        if self.uses_server_time() and not hasattr(record, "server_time"):
+            record.server_time = self.formatTime(record, self.datefmt)
+
+        record.msg = msg
+        return super().format(record)
+
+    def uses_server_time(self):
+        return self._fmt.find("{server_time}") >= 0
+
+
+def log_response(
+    message,
+    *args,
+    response=None,
+    request=None,
+    logger=request_logger,
+    level=None,
+    exception=None,
+):
+    """
+    Log errors based on HttpResponse status.
+
+    Log 5xx responses as errors and 4xx responses as warnings (unless a level
+    is given as a keyword argument). The HttpResponse status_code and the
+    request are passed to the logger's extra parameter.
+    """
+    # Check if the response has already been logged. Multiple requests to log
+    # the same response can be received in some cases, e.g., when the
+    # response is the result of an exception and is logged when the exception
+    # is caught, to record the exception.
+    if getattr(response, "_has_been_logged", False):
+        return
+
+    if level is None:
+        if response.status_code >= 500:
+            level = "error"
+        elif response.status_code >= 400:
+            level = "warning"
+        else:
+            level = "info"
+
+    getattr(logger, level)(
+        message,
+        *args,
+        extra={
+            "status_code": response.status_code,
+            "request": request,
+        },
+        exc_info=exception,
     )
+    response._has_been_logged = True
