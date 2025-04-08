92f33a01667dcd0a52b536b80cdf2f5eef4b5897
drefas <drefasgt@gmail.com>
2025-04-07 21:52:45 -0700

    Add files via upload

--- apps.py
+++ apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class RedirectsConfig(AppConfig):
+class SiteMapsConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
-    name = "django.contrib.redirects"
-    verbose_name = _("Redirects")
+    name = "django.contrib.sitemaps"
+    verbose_name = _("Site Maps")
