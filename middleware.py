703149cafa99ce06fb04ad8c498cc2b0bccdf384
drefas <drefasgt@gmail.com>
2025-04-07 21:42:16 -0700

    Add files via upload

--- middleware.py
+++ middleware.py
@@ -1,26 +1,50 @@
+from django.apps import apps
 from django.conf import settings
-from django.contrib.messages.storage import default_storage
+from django.contrib.redirects.models import Redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.exceptions import ImproperlyConfigured
+from django.http import HttpResponseGone, HttpResponsePermanentRedirect
 from django.utils.deprecation import MiddlewareMixin
 
 
-class MessageMiddleware(MiddlewareMixin):
-    """
-    Middleware that handles temporary messages.
-    """
+class RedirectFallbackMiddleware(MiddlewareMixin):
+    # Defined as class-level attributes to be subclassing-friendly.
+    response_gone_class = HttpResponseGone
+    response_redirect_class = HttpResponsePermanentRedirect
 
-    def process_request(self, request):
-        request._messages = default_storage(request)
+    def __init__(self, get_response):
+        if not apps.is_installed("django.contrib.sites"):
+            raise ImproperlyConfigured(
+                "You cannot use RedirectFallbackMiddleware when "
+                "django.contrib.sites is not installed."
+            )
+        super().__init__(get_response)
 
     def process_response(self, request, response):
-        """
-        Update the storage backend (i.e., save the messages).
+        # No need to check for a redirect for non-404 responses.
+        if response.status_code != 404:
+            return response
 
-        Raise ValueError if not all messages could be stored and DEBUG is True.
-        """
-        # A higher middleware layer may return a request which does not contain
-        # messages storage, so make no assumption that it will be there.
-        if hasattr(request, "_messages"):
-            unstored_messages = request._messages.update(response)
-            if unstored_messages and settings.DEBUG:
-                raise ValueError("Not all temporary messages could be stored.")
+        full_path = request.get_full_path()
+        current_site = get_current_site(request)
+
+        r = None
+        try:
+            r = Redirect.objects.get(site=current_site, old_path=full_path)
+        except Redirect.DoesNotExist:
+            pass
+        if r is None and settings.APPEND_SLASH and not request.path.endswith("/"):
+            try:
+                r = Redirect.objects.get(
+                    site=current_site,
+                    old_path=request.get_full_path(force_append_slash=True),
+                )
+            except Redirect.DoesNotExist:
+                pass
+        if r is not None:
+            if r.new_path == "":
+                return self.response_gone_class()
+            return self.response_redirect_class(r.new_path)
+
+        # No redirect was found. Return the response.
         return response
