aa09fc62a2bb4e151e7f5cb9666fb0955c7a62e1
drefas <drefasgt@gmail.com>
2025-04-07 22:32:21 -0700

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,9 +1,52 @@
-"""Multi-consumer multi-producer dispatching mechanism
+from django.http.cookie import SimpleCookie, parse_cookie
+from django.http.request import (
+    HttpHeaders,
+    HttpRequest,
+    QueryDict,
+    RawPostDataException,
+    UnreadablePostError,
+)
+from django.http.response import (
+    BadHeaderError,
+    FileResponse,
+    Http404,
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseBase,
+    HttpResponseForbidden,
+    HttpResponseGone,
+    HttpResponseNotAllowed,
+    HttpResponseNotFound,
+    HttpResponseNotModified,
+    HttpResponsePermanentRedirect,
+    HttpResponseRedirect,
+    HttpResponseServerError,
+    JsonResponse,
+    StreamingHttpResponse,
+)
 
-Originally based on pydispatch (BSD) https://pypi.org/project/PyDispatcher/2.0.1/
-See license.txt for original license.
-
-Heavily modified for Django's purposes.
-"""
-
-from django.dispatch.dispatcher import Signal, receiver  # NOQA
+__all__ = [
+    "SimpleCookie",
+    "parse_cookie",
+    "HttpHeaders",
+    "HttpRequest",
+    "QueryDict",
+    "RawPostDataException",
+    "UnreadablePostError",
+    "HttpResponse",
+    "HttpResponseBase",
+    "StreamingHttpResponse",
+    "HttpResponseRedirect",
+    "HttpResponsePermanentRedirect",
+    "HttpResponseNotModified",
+    "HttpResponseBadRequest",
+    "HttpResponseForbidden",
+    "HttpResponseNotFound",
+    "HttpResponseNotAllowed",
+    "HttpResponseGone",
+    "HttpResponseServerError",
+    "Http404",
+    "BadHeaderError",
+    "JsonResponse",
+    "FileResponse",
+]
